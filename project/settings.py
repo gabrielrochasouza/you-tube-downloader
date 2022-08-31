2065f5ca6758c08ebb43288a864042d0e6242d5b
Gabriel <grsouza135@gmail.com>
2022-08-31 14:50:55 -0300

    refactor: settings static_root

--- project/settings.py
+++ project/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
